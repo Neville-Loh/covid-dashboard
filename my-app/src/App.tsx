8f62e06633cf08ec312f161baeeaeaf6366e3ad4
Neville <neville.code@gmail.com>
2020-07-24 15:52:25 +1200

    Added connectinon of Covid 19 API

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo_2.svg';
 import './App.css';
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+interface GlobalData{
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number;
+  TotalDeaths: number;
+  NewRecovered: number;
+  TotalRecovered: number;
+}
+interface CountryData{
+  country: String;
+  CountryCode: String;
+  Slug: String;
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number
+  TotalDeaths: number
+  NewRecovered: number;
+  TotalRecovered: number;
+  Date: String;
+}
+interface IState{
+  Global: GlobalData;
+  Countries: CountryData[];
+}
 
 function App() {
+
+  // Fetching data
+  const [latest, setLatest] = useState<IState | null>(null);
+  useEffect(() => {
+    axios
+      .get("https://api.covid19api.com/summary")
+      .then(res => {
+        setLatest(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+
+  if (latest != null){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MSA 2020 DevOps
-        </p>
-        <a
-          className="App-link"
-          href="https://neville-msa-devops-2020.azurewebsites.net/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MSA website.
-        </a>
-      </header>
+    <div>
+      <CardDeck>
+        <Card style ={{ margin: "10px"}}>
+
+          <Card.Body>
+            <Card.Title>Global Confirmed Cases</Card.Title>
+            <Card.Text>
+              {latest.Global.TotalConfirmed}
+      </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="danger" text={"white"} style ={{ margin: "10px"}}>
+          <Card.Body>
+            <Card.Title>Total Deaths</Card.Title>
+            <Card.Text>
+              {latest.Global.TotalDeaths}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {latest.Countries[0].Date}</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="success" text={"white"} style ={{ margin: "10px"}}>
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>
+              {latest.Global.TotalRecovered}
+      </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
     </div>
   );
+} else {
+  return (<div>
+    <h1>Very bad stuff happen</h1>
+  </div>)
+}
 }
 
 export default App;
